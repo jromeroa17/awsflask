4ee5a70ae6be6f0feffc91c6fc44f7a66bb54579
jromeroa17 <jromeroa17@educarex.es>
2025-03-19 10:59:16 +0100

    creada pagina de crear instancias

--- website/views.py
+++ website/views.py
@@ -7,3 +7,6 @@ views = Blueprint('views', __name__)
 def home():
     return render_template('home.html')
 
+@views.route('/createInstance', methods=['GET','POST'])
+def create_instance():
+    return render_template('createInstance.html')
