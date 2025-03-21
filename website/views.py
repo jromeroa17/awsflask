88010f79452d4c227d32511fd98e713196318f66
jromeroa17 <jromeroa17@educarex.es>
2025-03-21 10:11:54 +0100

    Creado formulario para el botox3

--- website/views.py
+++ website/views.py
@@ -1,8 +1,32 @@
 from flask import Blueprint, request, flash
 from flask import render_template
+import boto3
+import botocore.exceptions
 
 
+ec2 = boto3.client('ec2')
 views = Blueprint('views', __name__)
+
+def crearInstancia(name,opsys,keypair,instnum):
+    response = ec2.run_instances(
+        ImageId=amis[opsys],
+        InstanceType='t2.micro',
+        MinCount=1,
+        MaxCount=instnum,
+        KeyName=keypair,
+        TagSpecifications=[
+            {
+                'ResourceType': 'instance',
+                'Tags': [
+                    {
+                        'Key': 'Name',
+                        'Value': name
+                    },
+                ]
+            },
+        ]
+    )
+    return response
 
 @views.route('/')
 def home():
@@ -14,8 +38,20 @@ def create_instance():
         name = request.form.get('instanceName')
         opsys = request.form.get('so')
         keypair = request.form.get('keypair')
-        instnum = request.form.get('instnum')
+        instnum = int(request.form.get('instnum'))
 
-        flash(f'{name}: {opsys} la clave es {keypair} y hay que crear {instnum}')
+        try:
+            crearInstancia(name,opsys,keypair,instnum)
+        except botocore.exceptions.ClientError:
+            ec2.create_key_pair(KeyName=keypair)
+            crearInstancia(name, opsys, keypair, instnum)
 
     return render_template('createInstance.html')
+
+amis = {
+    'AmazonLinux':'ami-08b5b3a93ed654d19',
+    'Ubuntu':'ami-084568db4383264d4',
+    'WinServer':'ami-02e3d076cbd5c28fa',
+    'Debian':'ami-0779caf41f9ba54f0',
+}
+
